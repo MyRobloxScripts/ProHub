fb5ac5e1440503e25934a1003d8544725b6bbcb1
MyRobloxScripts <136196389+MyRobloxScripts@users.noreply.github.com>
2023-08-16 00:39:04 +0300

    Pro Hub Loader.lua

--- Pro Hub Loader.lua
+++ Pro Hub Loader.lua
@@ -1,6 +1,5 @@
 local games = {
     [{286090429}] = "https://raw.githubusercontent.com/MyRobloxScripts/ProHub/main/Pro%20Hub%20Arsenal.lua",
-    [{6839171747}] = "https://raw.githubusercontent.com/MyRobloxScripts/ProHub/main/Pro%20Hub.lua",
 }
 
 for ids, url in next, games do
